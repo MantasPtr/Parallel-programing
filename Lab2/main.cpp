899397cd07e1a296db9f8fe75e73c028fe45bd78
mantas.p <man.petrikas@gmail.com>
2017-11-04 16:32:36 +0200

    works

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -26,19 +26,33 @@ double GetTime() {
 int calculatePi (int outerLoop, int innerLoop) {
     int inCircle = 0;
     int total = 0;
+    double totalGentime = 0;
+    //unsigned int seed = (unsigned int) 1;      
     #pragma omp parallel 
     {
-        unsigned int seed[omp_get_num_threads()];
-        for (int fr= 0; fr<omp_get_num_threads(); fr++) {
-            seed[fr] = (unsigned int) fr; 
-        }
+        //printf("%d\n", omp_get_thread_num());
+        //srand(int(time(NULL)) ^ omp_get_thread_num());
+        //unsigned int seed[omp_get_num_threads()];
+        //for (int fr= 0; fr<omp_get_num_threads(); fr++) {
+        //    seed[fr] = (unsigned int) fr; 
+        //}
+        unsigned int seed = (unsigned int) omp_get_thread_num();
         for (int fr1 = 0; fr1 < outerLoop; fr1++) {
-            #pragma omp for reduction (+:inCircle, total)
+            #pragma omp for reduction (+:inCircle, total, totalGentime)
             for (int fr2 = 0; fr2 < innerLoop; fr2++) {
-                double x = getRandomDoubleWithoutSeed();
+                printf("%d %d \n",omp_get_thread_num() ,seed);
+                double gen1s = GetTime();
+                //double x = getRandomDoubleWithoutSeed();
                 //double x = getRandomDouble(&seed[omp_get_thread_num()]);
-                double y = getRandomDoubleWithoutSeed();
+                double x = getRandomDouble(&seed);
+                //double y = getRandomDoubleWithoutSeed();
                 //double y = getRandomDouble(&seed[omp_get_thread_num()]);
+                double y = getRandomDouble(&seed);
+                //printf("%.6f %.6f\n",x,y);
+                
+                double gen1f = GetTime();
+                totalGentime=gen1f-gen1s;
+                //printf("r1: %.9f r2: %.9f\n",gen1f-gen1s, gen2f-gen1f);
                 if (pow(x,2) + pow(y,2) < 1) {
                         inCircle++;
                 }
@@ -46,18 +60,17 @@ int calculatePi (int outerLoop, int innerLoop) {
             }
             #pragma omp master
             {
-                printf("total: %d, inCircle: %d ", total, inCircle);
+                printf("total: %d, inCircle: %d , totalGenTime: %.6f ", total, inCircle, totalGentime);
                 printf("pi %f\n", ((double)inCircle / total) * 4);
             }
         }
     }
 }
- 
 int main(int argc, char *argv[]){
-    omp_set_num_threads(1);
+    //omp_set_num_threads(1);
     srand(time(NULL));
     double startTime = GetTime();
-    calculatePi(100, 100000);
+    calculatePi(1, 12);
     double endTime = GetTime();
     printf("%.3f\n", endTime - startTime);
 }
