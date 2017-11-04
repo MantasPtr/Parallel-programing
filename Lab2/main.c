446c0315d23d285191d1f8df17e4e339b040981a
mantas.p <man.petrikas@gmail.com>
2017-11-04 12:58:58 +0200

    2.2

--- Lab2/main.c
+++ Lab2/main.c
@@ -6,20 +6,37 @@
 #define sqr(x) (x*x)
 #define randF (double)rand()/RAND_MAX
 
-int main(int argc, char *argv[]){
-    srand(time(NULL));
-    int total = 0;
+double GetTime() {
+    struct timeval laikas;
+    gettimeofday(&laikas, NULL);
+    double rez = (double)laikas.tv_sec+(double)laikas.tv_usec/1000000;
+    return rez;
+ } 
+
+int calculatePi (int outerLoop, int innerLoop) {
     int inCircle = 0;
-    int less = 0;
-    for (int fr1 = 0; fr1 < 100; fr1++) {
-        for (int fr2 = 0; fr2 < 1000000; fr2++) {
-            double d1 = randF;
-            double d2 = randF;
-            if (sqr(d1) + sqr(d2) < 1) {
+    int total = 0;
+    for (int fr1 = 0; fr1 < outerLoop; fr1++) {
+        #pragma omp parallel for //reduction(+:total)
+        for (int fr2 = 0; fr2 < innerLoop; fr2++) {
+            double x = randF;
+            double y = randF;
+            if (sqr(x) + sqr(y) < 1) {
                 inCircle++;
             }
             total++;
         }
-        printf("pi %f\n", (double)inCircle / total * 4);
+        #pragma omp parallel master 
+        {
+            printf("pi %f\n", ((double)inCircle / total) * 4);
+        }
     }
+}
+ 
+int main(int argc, char *argv[]){
+    srand(time(NULL));
+    double startTime = GetTime();
+    calculatePi(100, 10000000);
+    double endTime = GetTime();
+    printf("%.3f\n", endTime - startTime);
 }
