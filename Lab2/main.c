a19988860347dc8e75c8f3ae102ebb9fe6c08cd6
Mantas Petrikas <mape3071@linux1.mif>
2017-11-04 13:24:51 +0200

    asd

--- Lab2/main.c
+++ Lab2/main.c
@@ -11,16 +11,19 @@ double GetTime() {
     gettimeofday(&laikas, NULL);
     double rez = (double)laikas.tv_sec+(double)laikas.tv_usec/1000000;
     return rez;
- } 
+ }
 
 int calculatePi (int outerLoop, int innerLoop) {
     int inCircle = 0;
     int total = 0;
     int fr1;
     for (fr1 = 0; fr1 < outerLoop; fr1++) {
-        #pragma omp parallel for //reduction(+:total)
-        int fr2;
+	int fr2;
+        #pragma omp parallel for // reduction (+:inCircle, total)
         for (fr2 = 0; fr2 < innerLoop; fr2++) {
+	    if(fr2 == 1000) {
+		printf("asd %d", omp_get_thread_num());
+	    }
             double x = randF;
             double y = randF;
             if (sqr(x) + sqr(y) < 1) {
@@ -28,10 +31,12 @@ int calculatePi (int outerLoop, int innerLoop) {
             }
             total++;
         }
-        #pragma omp parallel master 
-        {
-            printf("pi %f\n", ((double)inCircle / total) * 4);
-        }
+//        #pragma omp master
+//        {
+
+//	    printf("total: %d, inCircle: %d ", total, inCircle);
+  //          printf("pi %f\n", ((double)inCircle / total) * 4);
+  //      }
     }
 }
  
