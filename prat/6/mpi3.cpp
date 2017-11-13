8e66a8fd86108b7b01d830f69ad95d993999c32a
mantas.p <man.petrikas@gmail.com>
2017-11-13 23:13:05 +0200

    added proper debuging and output

--- prat/6/mpi3.cpp
+++ prat/6/mpi3.cpp
@@ -1,11 +1,18 @@
 #include <mpi.h>
 #include <stdio.h>
 
-//#define debug=true;
+//#define debug 
+
+#ifdef debug
+#define D(a) a
+#else
+#define D(a)
+#endif
+#define bufferSize 32
 
 int main(int argc, char* argv[]) {
     int rank, procCount;
-    int buff[32];
+    int buff[bufferSize];
     MPI_Status stat;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -13,33 +20,28 @@ int main(int argc, char* argv[]) {
     int recipientSize = 1;
     int sendSize = 16;
     if (rank == 0) {
-        for (int i = 0; i < 32; i++) {
+        for (int i = 0; i < bufferSize; i++) {
             buff[i] = i;
         }
     }
     while (recipientSize != procCount) {
-        //printf("     rank: %d array %d, %d, %d, %d\n", rank, buff[0], buff[1], buff[2], buff[3]);
         if (rank < recipientSize) {
-            #ifdef debug
-            printf("%d sending to %d\n", rank, rank + recipientSize);
-            #endif
-            MPI_Send(buff+sendSize, sendSize, MPI_INT, rank + recipientSize, 0, MPI_COMM_WORLD);
-            #ifdef debug
-            printf(" %d send to %d(%d)\n", rank, rank + recipientSize, *(buff+sendSize));
-            #endif
+            D(printf("%d sending to %d\n", rank, rank + recipientSize));
+            MPI_Send(buff + sendSize, sendSize, MPI_INT, rank + recipientSize, 0, MPI_COMM_WORLD);
+            D(printf(" %d send to %d (%d)\n", rank, rank + recipientSize, *(buff + sendSize)));
         }
         if (rank > recipientSize - 1 && rank <= recipientSize * 2 - 1) {
-            #ifdef debug
-            printf(" %d receiving form  %d\n", rank, rank - recipientSize);
-            #endif
+            D(printf(" %d receiving form  %d\n", rank, rank - recipientSize));
             MPI_Recv(buff, sendSize, MPI_INT, rank - recipientSize, 0, MPI_COMM_WORLD, &stat);
-            #ifdef debug
-            printf("  %d received form  %d (%d)\n", rank, rank - recipientSize, buff[0]);
-            #endif
+            D(printf("  %d received form  %d (%d)\n", rank, rank - recipientSize, buff[0]));
         }
         recipientSize *= 2;
         sendSize /= 2;
     }
-    printf("rank: %d array %d, %d, %d, %d\n", rank, buff[0], buff[1], buff[2], buff[3]);
+    printf("rank: %d array: %3d", rank, buff[0]);
+    for (int i = 1; i < sendSize * 2; i++) {
+        printf(", %3d", buff[i]);
+    }
+    printf("\n");
     MPI_Finalize();
 }
