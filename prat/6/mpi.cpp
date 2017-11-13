ba67f5cc77ce2ccdef1507d886fb415fcb43e60c
mantas.p <man.petrikas@gmail.com>
2017-11-13 19:58:12 +0200

    fixed mpi message sending and receiving

--- prat/6/mpi.cpp
+++ prat/6/mpi.cpp
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char *argv[]) {
-    int rank, value = 0;
-    printf("---");
+int main(int argc, char *argv[])
+{
+    int rank, value = 0, procCount;
     MPI_Status stat;
-    MPI_Init(&argc , &argv);
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if (rank == 0) {
+    MPI_Comm_size(MPI_COMM_WORLD, &procCount);
+    printf("rank:%d procCount:%d value:%d\n", rank, procCount, value);
+    if (rank == 0)
+    {
         value = 12;
-        printf("1. rank: %d, value: %d", rank, value);
-        MPI_Send(&value, 1, MPI_INT, rank, 0, MPI_COMM_WORLD);
-    } else {
-        printf("2. rank: %d, value: %d", rank, value);
-        MPI_Recv(&value, 1, MPI_INT, rank, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+        printf("sending\n");
+        MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
     }
-    printf("3. rank: %d value: %d", rank, value);
+    else
+    {
+        printf("receiving\n");
+        MPI_Recv(&value, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+    }
+    printf("rank:%d value:%d\n", rank, value);
     MPI_Finalize();
 }
