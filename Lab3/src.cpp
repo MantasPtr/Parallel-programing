24b1df7855adf62f96834af3afe314ebed9a0b96
mantas.p <man.petrikas@gmail.com>
2017-12-02 10:32:19 +0200

    added gather + refactored

--- Lab3/src.cpp
+++ Lab3/src.cpp
@@ -4,11 +4,19 @@
 #include <sys/time.h>
 #include <time.h>
 
+#define debug
 #ifdef debug
 #define D(a) a
 #else
 #define D(a)
 #endif
+#define blockCount 4
+
+double GetTime() {
+    struct timeval laikas;
+    gettimeofday(&laikas, NULL);
+    return (double)laikas.tv_sec+(double)laikas.tv_usec/1000000;
+ }
 
 void genMatrix(int *A, int N, int M) {
     // Clean matrix
@@ -16,35 +24,16 @@ void genMatrix(int *A, int N, int M) {
         A[i] = 0;
     }
     // Generate matrix
-    int m = M / 4;
+    int m = M / blockCount;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < m; j++) A[i * M + j] = (int)((double)rand() / RAND_MAX * 99) + 1;
-        if (i > 0 && (i + 1) % (N / 4) == 0) m += M / 4;
+        if (i > 0 && (i + 1) % (N / blockCount) == 0) m += M / blockCount;
     }
 }
 
-int main(int argc, char* argv[]) {
-    int id, procCount; 
-    int N = 16;
-    int M = 20;
-    int *A = new int[N * M];
-    int t, n;
-    double *median = new double[N];
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &id);
-    MPI_Comm_size(MPI_COMM_WORLD, &procCount);
-    int *workMatrix = new int[N*M/procCount];
-    if (id == 0) {
-        srand(time(NULL));
-        genMatrix(A, N, M);
-    }
-    MPI_Scatter(A           ,N*M/4/procCount, MPI_INT, workMatrix                  , N*M/4/procCount, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Scatter(A+N*M/4     ,N*M/4/procCount, MPI_INT, workMatrix+N*M/4/procCount  , N*M/4/procCount, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Scatter(A+N*M/4*2   ,N*M/4/procCount, MPI_INT, workMatrix+N*M/4/procCount*2, N*M/4/procCount, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Scatter(A+N*M/4*3   ,N*M/4/procCount, MPI_INT, workMatrix+N*M/4/procCount*3, N*M/4/procCount, MPI_INT, 0, MPI_COMM_WORLD);
-    
-
-    for (int rowNo = 0; rowNo < N/procCount; rowNo++) {
+void calculateMedian(int M, int oneBlockSize, int procCount, int* workMatrix, double* median){
+     int t,n;
+     for (int rowNo = 0; rowNo < oneBlockSize; rowNo++) {
         n = 0;
         while (workMatrix[rowNo * M + n] != 0 && n < M) {
             n++;
@@ -65,22 +54,50 @@ int main(int argc, char* argv[]) {
         }
     }
 
+}
 
-
-// Print matrix
-for (int j=0; j< procCount;j++) {
-    if (id == j) {
-        printf("id= %d\n",id);
-        for (int i = 0; i < N/procCount; i++) {
-            printf("median = %3.3f", median[i]);
-            for (int j = 0; j < M; j++) {
-                printf("%3d", workMatrix[i * M + j]);
+int main(int argc, char* argv[]) {
+    double startTime = GetTime();
+    int id, procCount; 
+    int N = 16;
+    int M = 20;
+    int *Matrix = new int[N * M];
+    double *median;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    MPI_Comm_size(MPI_COMM_WORLD, &procCount);
+    int *workMatrix = new int[N*M/procCount/blockCount];
+    int oneBlockSize = N/procCount/blockCount;
+    if (id == 0) {
+        srand(time(NULL));
+        genMatrix(Matrix, N, M);
+        median = new double[N];
+    } else {
+        median = new double[N/blockCount];
+    }
+    for (int i=0;i<blockCount;i++) {
+        MPI_Scatter(Matrix+N*M/blockCount*i     ,M*oneBlockSize, MPI_INT, workMatrix, M*oneBlockSize, MPI_INT, 0, MPI_COMM_WORLD);
+        calculateMedian(M, oneBlockSize, procCount, workMatrix, median);
+        MPI_Gather(median, oneBlockSize, MPI_DOUBLE, median+N/blockCount*i, oneBlockSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    }
+    MPI_Finalize();
+    double endTime = GetTime();
+    if (id == 0) {
+        printf("-------------------/n time: %.3f\n-------------------/n" , endTime - startTime);
+    }
+    if (id == 0) {  
+        for (int j=0; j< N;j++) {
+            for (int i = 0; i < M; i++) {
+                printf("%3d ",Matrix[j*M+i]);            
             }
-            printf("\n");
+        printf("\n");   
+        }   
+    }
+    if (id == 0) {
+        printf("Hello from master\n");
+        for (int i = 0; i < N; i++) {
+            printf("%3.3f\n", median[i]);
         }
     }
-    MPI_Barrier(MPI_COMM_WORLD);
-}
-    MPI_Finalize();
 
 }
